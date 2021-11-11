acff628f92c90c395f53e7fa6d593c249a467d95
herdiann <93063524+herdiann@users.noreply.github.com>
2021-11-11 09:34:12 +0700

    Update cs.php

--- cs.php
+++ cs.php
@@ -1,6 +1,6 @@
 <script>
 <button onclick="var jsVar = "<?php 
-$nomor = array("082115166509","081311742709"); 
+$nomor = array("+6282115166509","+6281311742709"); 
 $nohp = $nomor[array_rand($nomor)]; 
 $text = "Hi kak saya mau konsultasi atau beli *GOAMI GLUTASKIN* nya, Ready ?";
 header("Location: https://api.whatsapp.com/send?phone=$nohp&text=$text"); ?>"">
